2d0774c3df874f6c72adfaebcdfc0eb5e16e0e2a
Roberto Leite <rlajunior@gmail.com>
2019-10-08 16:27:11 -0300

    Final.

--- locaapp/src/main/java/br/edu/infnet/locaapp/app/model/Sala.java
+++ locaapp/src/main/java/br/edu/infnet/locaapp/app/model/Sala.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name="locacao")
 public class Sala {
 	
-	@Id 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
